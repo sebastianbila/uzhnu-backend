c823360e44041d322834d63233d3d43bc4746e67
Sebastian Bila <sebastianbila.sw@gmail.com>
2020-12-16 17:44:14 +0200

    Added pagination for news

--- src/api/controllers/newsController.js
+++ src/api/controllers/newsController.js
@@ -3,12 +3,30 @@ const { ok } = require('server-response')
 
 async function getAllNews(req, res, next) {
   try {
-    const { limit } = req.query
+    const { limit, page = 1 } = req.query
 
     const newsService = new ServiceFactory().createNewsService()
-    const news = await newsService.getAllNews(limit)
 
-    ok(res, news)
+    const result = {}
+    result.news = await newsService.getAllNews()
+
+    if (limit && limit !== 0) {
+      const totalItems = result.news.length
+      const pageSize = +limit
+      const currentPage = +page
+
+      result.pagination = {
+        totalItems,
+        totalPages: Math.ceil(totalItems / pageSize),
+        currentPage
+      }
+
+      if (!pageSize !== 0) {
+        result.news = result.news.slice((currentPage - 1) * pageSize).slice(0, pageSize)
+      }
+    }
+
+    ok(res, result)
   } catch (err) {
     next(err)
   }

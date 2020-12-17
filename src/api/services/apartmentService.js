0432424208371f5f422c5119a1887786db71d6a7
Sebastian Bila <sebastianbila.sw@gmail.com>
2020-12-17 09:29:13 +0200

    Update apartment

--- src/api/services/apartmentService.js
+++ src/api/services/apartmentService.js
@@ -2,10 +2,17 @@ const { Apartment } = require('../../db/models')
 
 class ApartmentService {
   async getApartmentName(name) {
-    const apartment = await Apartment.findOne({owner: name.trim()})
+    const apartment = await Apartment.find()
 
-    if (!apartment) throw new Error('Not found')
-    return apartment
+    let specificApartment
+    const isResident = apartment.map((apart) => {
+      if (apart.residents.some((i) => i === name)) {
+        specificApartment = apart
+      }
+    })
+
+    if (!specificApartment) throw new Error('Not found')
+    return specificApartment
   }
 }
 

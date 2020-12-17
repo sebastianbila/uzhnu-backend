8ee6ae4efe7260af89df2466e3e2bdcccaee73e2
Sebastian Bila <sebastianbila.sw@gmail.com>
2020-12-17 19:55:42 +0200

    Update apartment.js

--- src/db/models/apartment.js
+++ src/db/models/apartment.js
@@ -5,67 +5,11 @@ const schema = new Schema({
   accountNumber: { type: String },
   area: { type: String },
   adress: { type: String },
-  benefits: String,
+  benefits: { type: String },
   phoneNumber: { type: String },
-  services: {
-    electricityCost: {
-      type: String
-    },
-    trashCost: {
-      type: String
-    },
-    liftCost: {
-      type: String
-    },
-    quarterCost: {
-      type: String
-    },
-    gasCost: {
-      type: String
-    },
-    waterCost: {
-      type: String
-    },
-    electricityType: {
-      type: String
-    },
-    trashType: {
-      type: String
-    },
-    liftType: {
-      type: String
-    },
-    quarterType: {
-      type: String
-    },
-    gasType: {
-      type: String
-    },
-    waterType: {
-      type: String
-    },
-    electricity: {
-      type: String
-    },
-    water: {
-      type: String
-    },
-    trash: {
-      type: String
-    },
-    lift: {
-      type: String
-    },
-    quarter: {
-      type: String
-    },
-    gas: {
-      type: String
-    },
-    date: { type: Date, default: Date.now }
-  },
+  services: [],
+  lastIndicators: [],
   residents: [String]
 })
 
 module.exports = model('apartments', schema)
-
